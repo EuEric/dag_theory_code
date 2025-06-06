9f2a27ce44804a99983005a6d431398065af58d1
Eric <ericabraham8@yahoo.com>
2025-06-06 16:31:49 +0200

    allow to parse an entire folder of yaml files from input

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <filesystem> // For C++17 filesystem
 #include "assign_priority.h"
 #include "compute_length.h"
 #include "graph.h"
@@ -8,165 +9,136 @@
 #include "dag_parser.h"
 #include "utils.h"
 
+namespace fs = filesystem;
 using namespace std;
 
-int main(int argc, char* argv[]) {
-    int nr_proc;
-    DAGParser dagParser;
-    vector<Graph> dags;
-    std::string filename;
-    
-    // Default configuration
-    nr_proc = 2;
-
-    // Argument parser
-    for (int i = 1; i < argc; ++i) {
-        std::string arg = argv[i];
-
-        if ((arg == "-f" || arg == "--file") && i + 1 < argc) {
-            filename = argv[++i];
-        } else if ((arg == "--proc") && i + 1 < argc) {
-            nr_proc = std::stoi(argv[++i]);
-        }
+// Function to process a single YAML file and save data in a vector
+void process_yaml_file(const fs::path& path, DAGParser& dagParser, vector<pair<string, Graph>>& dags) {
+    cout << "Processing file: " << path << "\n";
+    vector<Graph> yaml_dags;
+    dagParser.readMultipleDAGSFromYAML(path.string(), yaml_dags);
+    for (auto& dag : yaml_dags) {
+        dags.emplace_back(path.string(), move(dag));
+    }
 }
-    if (!filename.empty()){
-        cout << "Reading DAG(s) from file: " << filename << "\n";
 
-        if (filename.find(".yaml") != std::string::npos || filename.find(".yml") != std::string::npos) {
-            dagParser.readMultipleDAGSFromYAML(filename, dags);    
-        } else if (filename.find(".txt") != std::string::npos) {
-            cout << "dot";
-            dagParser.readMultipleDAGSFromDOT(filename, dags);
-        } else {
-            std::cerr << "Unsupported file format. Use .dot or .yaml/.yml.\n";
-            return 1;
-        }
-    } else {
-        std::cerr << "Usage:\n"
-                  << argv[0] << " -f <taskset.yaml>\n"
-                  << "    OR\n"
-                  << argv[0] << " -g [-n <num_dags>]\n";
+// Function that processes input folder/yaml and saves data in a vector
+int process_input_path(const fs::path& input_path, DAGParser& dagParser, vector<pair<string, Graph>>& dags) {
+    if (!fs::exists(input_path)) {
+        cerr << "Input path does not exist.\n";
         return 1;
     }
 
+    if (fs::is_regular_file(input_path)) {
+        cout << "Reading DAG(s) from file: " << input_path << "\n";
+        
+        if (input_path.extension() == ".yaml" || input_path.extension() == ".yml") {
+            process_yaml_file(input_path, dagParser, dags);
+        } else {
+            cerr << "Unsupported file format. Use .yaml or .yml.\n";
+            return 1;
+        }
+    } 
+    else if (fs::is_directory(input_path)) {
+        cout << "Reading DAG(s) from directory: " << input_path << "\n";
 
-    // Define WCETs
+        for (const auto& entry : fs::directory_iterator(input_path)) {
+            if (entry.is_regular_file()) {
+                const auto& path = entry.path();
+                if (path.extension() == ".yaml" || path.extension() == ".yml") {
+                    process_yaml_file(path, dagParser, dags);
+                }
+            }
+        }
+    } 
+    else {
+        cerr << "Input path is neither a file nor a directory.\n";
+        return 1;
+    }
 
-    // Paper example
-    // vector<Task> tasks = {
-    //     {0, 1}, {1, 3}, {2, 1}, {3, 2}, {4, 1}, {5, 1}, {6, 1}
-    // };
+    return 0;
+}
 
-    // // Ray example
-    // vector<Task> tasks = {
-    //     {0, 0}, {1, 6}, {2, 4}, {3, 4}, {4, 15}, {5, 3}, {6, 0}
-    // };
+int main(int argc, char* argv[]) {
+    int nr_proc = 2;
+    DAGParser dagParser;
+    string input_path;
+    string output_dir = "output_files"; // Default output directory
 
-    // First example paper
-    // vector<Task> tasks = {
-    //     {0, 1}, {1, 4}, {2, 2}, {3, 2}, {4, 1}
-    // };
+    // Argument parser
+    for (int i = 1; i < argc; ++i) {
+        string arg = argv[i];
 
-    //int n = tasks.size(); // Number of vertices (Tasks)
+        if ((arg == "-i" || arg == "--input") && i + 1 < argc) {
+            input_path = argv[++i];
+        } else if ((arg == "-o" || arg == "--output") && i + 1 < argc) {
+            output_dir = argv[++i];
+        } else if ((arg == "--proc") && i + 1 < argc) {
+            nr_proc = stoi(argv[++i]);
+        }
+    }
 
-    // Paper example
-    // G.add_edge(0, 1);
-    // G.add_edge(0, 2);
-    // G.add_edge(0, 3);
-    // G.add_edge(1, 4);
-    // G.add_edge(2, 4);
-    // G.add_edge(3, 6);
-    // G.add_edge(4, 5);
-    // G.add_edge(5, 6);
+    cout << "Output directory: " << output_dir << '\n';
 
+    if (input_path.empty()) {
+        cerr << "Usage:\n"
+                  << argv[0] << " -i <input_path> [-o <output_dir>] [--proc <nr_proc>]\n"
+                  << "  input_path: a YAML file or a directory containing YAML files\n"
+                  << "  output_dir: directory to save the priority YAML files\n";
+        return 1;
+    }
 
-    // Ray example
-    // G.add_edge(0, 1);
-    // G.add_edge(0, 4);
-    // G.add_edge(1, 2);
-    // G.add_edge(1, 3);
-    // G.add_edge(2, 5);
-    // G.add_edge(3, 5);
-    // G.add_edge(4, 6);
-    // G.add_edge(5, 6);
-    // G.add_indirect_edge(1, 5);
+    if (!fs::exists(output_dir)) {
+        fs::create_directories(output_dir);
+    }
 
-    // First example paper
-    // G.add_edge(0, 1);
-    // G.add_edge(0, 2);
-    // G.add_edge(0, 3);
-    // G.add_edge(1, 4);
-    // G.add_edge(2, 4);
-    // G.add_edge(3, 4);
+    vector<pair<string, Graph>> dags;
 
-    // Big example Ray, on my paper
-    // G.add_edge(0, 1);
-    // G.add_edge(0, 2);
-    // G.add_edge(0, 3);
-    // G.add_edge(1, 4);
-    // G.add_edge(1, 5);
-    // G.add_edge(2, 6);
-    // G.add_edge(2, 7);
-    // G.add_edge(2, 8);
-    // G.add_edge(3, 12);
-    // G.add_edge(4, 9);
-    // G.add_edge(5, 9);
-    // G.add_edge(6, 10);
-    // G.add_edge(7, 10);
-    // G.add_edge(8, 10);
-    // G.add_edge(9, 11);
-    // G.add_edge(10, 12);
-    // G.add_edge(11, 12);
-    // G.add_indirect_edge(1, 9);
-    // G.add_indirect_edge(9, 11);
-    // G.add_indirect_edge(2, 10);
+    // Process either entire folder or a yaml file
+    process_input_path(input_path, dagParser, dags);
 
-    // // Big example Ray, on my paper
-    // G.tasks = {
-    //     {0, 0}, {1, 7}, {2, 5}, {3, 10}, {4, 4},
-    //     {5, 8}, {6, 8}, {7, 2}, {8, 1}, {9, 1},
-    //     {10, 3}, {11, 1}, {12, 0}
-    // };
-
-    cout << "dags size: " << dags.size() << " \n";
-
-    for(int i = 0; i < dags.size(); i++) {
-        int n = dags[i].tasks.size(); // Number of vertices (Tasks)
+    // Main algorithm loop
+    for (const auto& [filepath, dag] : dags) {
+        int n = dag.tasks.size();
 
         vector<int> lf(n, 0), lb(n, 0), l(n, 0);
 
         // Compute l, lf, and lb values for every vertex
-        compute_length(dags[i], lf, lb, l);
+        compute_length(dag, lf, lb, l);
 
         // Display computed length values
         cout << "Task\tWCET\tlf\tlb\tl\n";
         for (int j = 0; j < n; ++j) {
-            cout << j << "\t" << dags[i].tasks[j].wcet << "\t" << lf[j] << "\t" << lb[j] << "\t" << l[j] << "\n";
+            cout << j << "\t" << dag.tasks[j].wcet << "\t" << lf[j] << "\t" << lb[j] << "\t" << l[j] << "\n";
         }
 
-        // Initialize priority list and counter, mark as uninitialized with -1
         vector<int> priority(n, -1);
         int p = 0;
         float final_rt;
 
-        // Assign priorities
-        assign_priority(dags[i], l, lb, priority, p);
+        // Assign priorities to edges using He algorithm
+        assign_priority(dag, l, lb, priority, p);
 
-        //Check what is the RT before another round of assign
-        final_rt = compute_rt_bound(dags[i], priority, nr_proc);
+        // Check RT bound for He algorithm priorities
+        final_rt = compute_rt_bound(dag, priority, nr_proc);
         cout << "RT bound BEFORE COMPUTING NEW PRIORITIES: " << final_rt << "\n";
 
-        // Assign 'correct' priorities to fork-join nodes
-        assign_indirect_priority(dags[i], priority);
+        // Use our method to propagate priorities on indirect edges
+        assign_indirect_priority(dag, priority);
 
         cout << "Assigned priorities:\n";
         for (int j = 0; j < n; ++j) {
             cout << "Task " << j << ": priority = " << priority[j] << "\n";
         }
 
-        dump_priorities_to_yaml("priorities_" + extract_filename(filename), priority);
+        string filename = fs::path(filepath).stem().string();
+        fs::path output_path = fs::path(output_dir) / ("priorities_" + filename + ".yaml");
+        dump_priorities_to_yaml(output_path, priority);
 
-        final_rt = compute_rt_bound(dags[i], priority, nr_proc);
+        cout << "Saved priorities to: " << output_path.string() << "\n";
+
+        // Check RT bound after our priority assignment
+        final_rt = compute_rt_bound(dag, priority, nr_proc);
         cout << "RT bound AFTER COMPUTING NEW PRIORITIES: " << final_rt << "\n";
     }
 
