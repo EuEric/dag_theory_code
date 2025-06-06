caf2c0ef94bd2c5e8f36ffff500470a39f25faa0
Eric <ericabraham8@yahoo.com>
2025-06-06 14:49:00 +0200

    adapt priority dump util to take a full path

--- include/utils.h
+++ include/utils.h
@@ -2,10 +2,14 @@
 
 #include <iostream>
 #include <fstream>
+#include <filesystem>
 #include <vector>
 
+namespace fs = std::filesystem;
+
 // Used to extrace the YAML filename from a command line input
+// No longer needed with C++17 version
 std::string extract_filename(const std::string& path);
 
 // Dump priority vector to a YAML file
-void dump_priorities_to_yaml(const std::string& file_name, const std::vector<int>& priority);
+void dump_priorities_to_yaml(const fs::path& path, const std::vector<int>& priority);
--- src/utils.cpp
+++ src/utils.cpp
@@ -1,5 +1,6 @@
 #include "utils.h"
 
+// No longer needed with C++17 version
 std::string extract_filename(const std::string& path) {
     size_t pos = path.find_last_of("/\\");
     if (pos == std::string::npos)
@@ -7,19 +8,24 @@ std::string extract_filename(const std::string& path) {
     return path.substr(pos + 1);
 }
 
-void dump_priorities_to_yaml(const std::string& file_name, const std::vector<int>& priority) {
-    std::ofstream fout("output_files/" + file_name);
-    if (!fout.is_open()) {
-    std::cerr << "Failed to open output file: output_files/" << file_name << "\n";
-    return;
+void dump_priorities_to_yaml(const fs::path& path, const std::vector<int>& priority) {
+    std::ofstream fout(path);
+    if (!fout) {
+        std::cerr << "Failed to open output file: " << path << "\n";
+        return;
     }
 
-    fout << "priorities:\n";
-    int n = priority.size();
-
-    for (int j = 0; j < n; ++j) {
-        fout << "  - id: " << j << "\n";
-        fout << "    priority: " << priority[j] << "\n";
+    // Write YAML content
+    try {
+        fout << "priorities:\n";
+        int n = priority.size();
+        
+        for (int j = 0; j < n; ++j) {
+            fout << "  - id: " << j << "\n"
+                 << "    priority: " << priority[j] << "\n";
+        }
+    } catch (const std::ios_base::failure& e) {
+        std::cerr << "Error writing to file: " << e.what() << "\n";
     }
     fout.close();
 }
